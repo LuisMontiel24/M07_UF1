f231004c3bda4e9887731c23cfd946500f808d6f
Angel Montiel <2024_luis.montiel@iticbcn.cat>
2024-11-10 19:39:27 +0100

    establece una conexión a una base de datos PostgreSQL

--- ACTIVITAT7/connection.py
+++ ACTIVITAT7/connection.py
@@ -1,16 +1,11 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    database="postgres",
-    user="luis",
-    password="123",
-    host="localhost",
-    port="5432"
-)
-
-cursor = conn.cursor()
-print(cursor)
-
-cursor.close()
-conn.close()
-
+def connect():
+    conn = psycopg2.connect(
+        database="postgres",
+        user="luis",
+        password="123",
+        host="localhost",
+        port="5432"
+    )
+    return conn
